c2682e064fa2627393a6f49a3bbd6f893f771d6d
saudjavaid2003 <saudjavaid2003@users.noreply.github.com>
2026-01-21 03:06:45 -0800

    profile controllers

--- models/studentProfile.model.js
+++ models/studentProfile.model.js
@@ -1,19 +1,23 @@
-import mongoose from "mongoose";
-
 const StudentProfileSchema = new mongoose.Schema({
-  userId:     { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   degree: { type: String, default: "BSCS" },
-  semester:   { type: Number },
-  cgpa:       { type: Number, min: 0, max: 4.0 },
+  semester: { type: Number }, // current semester
+  cgpa: { type: Number, min: 0, max: 4.0 },
 
   technicalSkills: [{ type: String }],
-  softSkills:      [{ type: String }], 
+  softSkills: [{ type: String }],
 
-  academics: [{
-    course: String,
-    grade: String,
-    score: Number, // optional numeric score
-  }],
+  academics: [
+    {
+      semester: { type: Number, required: true },
+      courses: [
+        {
+          course: { type: String, required: true },
+          grade: { type: String },
+          score: { type: Number }, // optional numeric score
+        },
+      ],
+    },
+  ],
 }, { timestamps: true });
-
 export default mongoose.model("StudentProfile", StudentProfileSchema);
